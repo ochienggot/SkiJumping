9b34291eb7e575428d8b7fe44ddcd08ffe85509b
Nicholas Got <nicholas.got@gmail.com>
2015-11-15 05:48:45 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Write your main program here. Implementing your own classes will be very useful.
 	    Scanner reader = new Scanner(System.in);
-	    // Create new tournament
+	    
 	    SkiJumpingTournament tourna = new SkiJumpingTournament(reader);
 
 	    tourna.start();
